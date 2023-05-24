8b49c3f379a6f06d8ee512705b2205ae32358006
Ashad Naushad <mohda170602@gmail.com>
2023-05-24 17:47:31 +0530

    Organised

--- app/src/main/java/com/example/bottomnav/Fragments/LinksFragment.kt
+++ app/src/main/java/com/example/bottomnav/Fragments/LinksFragment.kt
@@ -1,10 +1,11 @@
-package com.example.bottomnav
+package com.example.bottomnav.Fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.bottomnav.R
 
 class LinksFragment : Fragment() {
 
@@ -16,7 +17,6 @@ class LinksFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_links, container, false)
 
-        val name = view
 
         return view
     }
