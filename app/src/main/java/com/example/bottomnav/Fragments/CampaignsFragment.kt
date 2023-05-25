71b45a14f842f0311f203b74b60d58ed088ac053
Ashad Naushad <mohda170602@gmail.com>
2023-05-25 23:33:42 +0530

    view model with binding

--- app/src/main/java/com/example/bottomnav/Fragments/CampaignsFragment.kt
+++ app/src/main/java/com/example/bottomnav/Fragments/CampaignsFragment.kt
@@ -22,7 +22,6 @@ class CampaignsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
 
     }
 }
