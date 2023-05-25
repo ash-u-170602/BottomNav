c334b6286aa623325e80a8c9e8b7c3dd798897bf
Ashad Naushad <mohda170602@gmail.com>
2023-05-25 20:35:42 +0530

    view model with binding

--- app/src/main/java/com/example/bottomnav/MainActivity.kt
+++ app/src/main/java/com/example/bottomnav/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.bottomnav
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavHostController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.bottomnav.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val bottomNavigationView = binding.bottomNavigation
         val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
         bottomNavigationView.setupWithNavController(navController.navController)
@@ -24,8 +27,6 @@ class MainActivity : AppCompatActivity() {
     fun floatingAction(view: View) {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
-
         navController.navigate(R.id.addFragment)
-
     }
 }
