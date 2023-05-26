be8ab4b9bc7322f263a2cf3bed188a816b68b47e
Ashad Naushad <mohda170602@gmail.com>
2023-05-26 20:44:33 +0530

    view model with binding

--- app/src/main/java/com/example/bottomnav/NewsApi/NewsService.kt
+++ app/src/main/java/com/example/bottomnav/NewsApi/NewsService.kt
@@ -1,9 +1,9 @@
 package com.example.bottomnav.NewsApi
 
+import com.example.bottomnav.NewsApi.modalClasses.News
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-//https://newsapi.org/v2/top-headlines?country=in&apiKey=180ee558b8a04d6d926acfc4b9cc2e59
 
 const val BASE_URL = "https://newsapi.org/"
 const val API_KEY = "180ee558b8a04d6d926acfc4b9cc2e59"
@@ -11,6 +11,6 @@ const val API_KEY = "180ee558b8a04d6d926acfc4b9cc2e59"
 interface NewsInterface {
 
     @GET("v2/top-headlines?apiKey=$API_KEY")
-    fun getHeadlines(@Query("country") country: String, @Query("page") page: Int)
+    fun getHeadlines(@Query("country") country: String, @Query("page") page: Int): Call<News>
 
 }
